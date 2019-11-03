bc6f6a56c3b93139e056adf59629996d35ae87da
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-11-03 21:05:10 +0900

    add: def perimeter

--- practice.py
+++ practice.py
@@ -4,11 +4,14 @@ class Circle:
         self.radius = radius
         self.area = self.radius ** 2 * self.exercise_rate
 
+        self.perimeter = (self.radius) * 2 * self.exercise_rate
+
     def area(self):
         return self.area
 
-
+    def perimeter(self):
+        return self.perimeter
 
 circle1 = Circle(radius=1)
-print(circle1.area)  # 3.14
-# print(circle1.perimeter())  # 6.28
+print(circle1.area)
+print(circle1.perimeter)
