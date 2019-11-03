5f6916b674328ef53ba5822b7ca8258531abebcb
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-11-03 20:43:48 +0900

    fix: resolution Todo

--- practice.py
+++ practice.py
@@ -2,13 +2,13 @@ class Circle:
     def __init__(self, radius):
         self.exercise_rate = 3.14
         self.radius = radius
+        self.area = self.radius ** 2 * self.exercise_rate
 
     def area(self):
-        self.area = self.radius ** 2 * self.exercise_rate
         return self.area
 
 
-# Todo 表示できない
+
 circle1 = Circle(radius=1)
 print(circle1.area)  # 3.14
 # print(circle1.perimeter())  # 6.28
