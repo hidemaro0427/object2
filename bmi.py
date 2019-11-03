9861d56efc9a20e00a207a53f7e85607e3d8e736
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-11-03 17:11:13 +0900

    add: class BMI
    example code

--- bmi.py
+++ bmi.py
@@ -13,24 +13,22 @@ class BMI:
         - ???
 """
 
-
 # クラス名はUpperCamelCaseが普通
 class BMI:
     def __init__(self, height, weight):
-        self.height = height
-        self.weight = weight
+        self.value = weight / (height ** 2)
 
-    def calculatebmi_bmi(self):
-        return self.weight / (self.height ** 2)
+        if not (10 <= self.value <= 0):
+            raise ValueError('BMIが正常値の範囲を超えています')
+
+    def __str__(self):
+        return f'{self.value:.2f}'
 
 
-# BMIクラスのインスタンス化
-hibiki_bmi = BMI(height=1.80, weight=67.0)
-print("HIBIKI")
-print(hibiki_bmi.height, hibiki_bmi.weight)
-print(hibiki_bmi.calculatebmi_bmi())
+hikibi_bmi = BMI(height=1.80, weight=67.0)
+print(hikibi_bmi)
 
-noriya_bmi = BMI(height=1.78, weight=75.0)
-print("NORIYA")
-print(noriya_bmi.height, noriya_bmi.weight)
-print(noriya_bmi.calculatebmi_bmi())
+ohira_bmi = BMI(height=1.78, weight=75.0)
+print(ohira_bmi)
+
+yabai_bmi = BMI(height=1.78, weight=75000.0)
