7a13815a7f4c919a5014e23f1a6d8fbe01b7d35d
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-11-05 09:20:23 +0900

    add: def rectangle2 area

--- rectangle.py
+++ rectangle.py
@@ -7,6 +7,7 @@ rectangle2 = Rectangle(height=3, width=3)
 rectangle2.area()  # 9.00
 rectangle2.diagonal()  # 4.24
 """
+import math
 
 
 class Rectangle:
@@ -17,7 +18,14 @@ class Rectangle:
     def area(self):
         return f'{self.height * self.width: .2f}'
 
+    def diagonal(self):
+        return f'{math.sqrt((self.height ** 2 + self.width ** 2)): .2f}'
+
 
 rectangle1 = Rectangle(height=5, width=6)
 print(rectangle1.area())
-# print(rectangle1.diagonal())
+print(rectangle1.diagonal())
+
+rectangle2 = Rectangle(height=3, width=3)
+print(rectangle2.area())
+print(rectangle2.diagonal())
